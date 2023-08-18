4e7c82c0ca5d52cdb44b8b4ece9031129bf54220
F-Holm <101185761+F-Holm@users.noreply.github.com>
2023-08-17 23:38:34 -0300

    funciona

--- src/ServidorBroker.java
+++ src/ServidorBroker.java
@@ -6,57 +6,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner;
 
 public class ServidorBroker {
     private static final int PUERTO = 12345;
     private static HashSet<ClienteHilo> clientes = new HashSet<>();
     public static HashMap<String, HashSet<ClienteHilo>> topicos = new HashMap<>();
-    private static boolean servidorActivo = true;
-
     public static void main(String[] args) {
         try {
             ServerSocket servidorSocket = new ServerSocket(PUERTO);
             System.out.println("Servidor Broker TCP SIN ACK iniciado en el puerto " + PUERTO);
-
-            Thread consoleThread = new Thread(() -> {
-                Scanner scanner = new Scanner(System.in);
-                while (servidorActivo) {
-                    String comando = scanner.nextLine();
-                    if (comando.equalsIgnoreCase("SALIR")) {
-                        servidorActivo = false;
-                        cerrarServidor();
-                    }
-                }
-                scanner.close();
-            });
-            consoleThread.start();
-
-            while (servidorActivo) {
+            while (true) {
                 Socket clienteSocket = servidorSocket.accept();
-                if (servidorActivo) {
-                    ClienteHilo cliente = new ClienteHilo(clienteSocket);
-                    clientes.add(cliente);
-                    cliente.start();
-                } else {
-                    clienteSocket.close();
-                }
+                ClienteHilo cliente = new ClienteHilo(clienteSocket);
+                clientes.add(cliente);
+                cliente.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    static synchronized void cerrarServidor() {
-        for (ClienteHilo cliente : clientes) {
-            cliente.enviarMensaje("Servidor", "El servidor se ha cerrado.");
-            cliente.finalizarHilo();
-        }
-        clientes.clear();
-        topicos.clear();
-    }
-
-
     static synchronized void enviarMensaje(String topico, String mensaje, ClienteHilo remitente) {
         HashSet<ClienteHilo> suscriptores = topicos.get(topico);
         if (suscriptores != null) {
@@ -81,19 +49,14 @@ public class ServidorBroker {
             }
         }
     }
-
     static synchronized void quitarCliente(ClienteHilo cliente) {
         clientes.remove(cliente);
     }
 }
-
 class ClienteHilo extends Thread {
     private Socket socket;
     private BufferedReader entrada;
     private PrintWriter salida;
-
-    private boolean hiloActivo = true;
-
     public ClienteHilo(Socket socket) {
         this.socket = socket;
         try {
@@ -103,21 +66,11 @@ class ClienteHilo extends Thread {
             e.printStackTrace();
         }
     }
-    public void finalizarHilo() {
-        hiloActivo = false;
-        try {
-            entrada.close();
-            salida.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     @Override
     public void run() {
         try {
             String mensaje;
-            while (hiloActivo && (mensaje = entrada.readLine()) != null) {
+            while ((mensaje = entrada.readLine()) != null) {
                 String[] partes = mensaje.split(":");
                 String contenido = partes.length > 2 ? partes[2] : "";
 
@@ -136,7 +89,9 @@ class ClienteHilo extends Thread {
                         }
                         ServidorBroker.quitarCliente(this);
                         enviarMensaje("Servidor", "Te has desconectado");
-                        finalizarHilo();
+                        entrada.close();
+                        salida.close();
+                        socket.close();
                         return;
                     default:
                         ServidorBroker.enviarMensaje(partes[0], partes[1], this);
@@ -147,7 +102,6 @@ class ClienteHilo extends Thread {
             e.printStackTrace();
         }
     }
-
     void enviarMensaje(String topico, String mensaje) {
         salida.println(topico + ": " + mensaje);
     }
