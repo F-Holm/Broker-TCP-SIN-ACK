b28e43662375ef1615570e8384e73a816fad37f2
F-Holm <101185761+F-Holm@users.noreply.github.com>
2023-08-17 23:04:55 -0300

    agregué la función para cerrar el servidor

--- src/ServidorBroker.java
+++ src/ServidorBroker.java
@@ -6,27 +6,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class ServidorBroker {
     private static final int PUERTO = 12345;
     private static HashSet<ClienteHilo> clientes = new HashSet<>();
     public static HashMap<String, HashSet<ClienteHilo>> topicos = new HashMap<>();
+    private static boolean servidorActivo = true;
 
     public static void main(String[] args) {
         try {
             ServerSocket servidorSocket = new ServerSocket(PUERTO);
             System.out.println("Servidor Broker TCP SIN ACK iniciado en el puerto " + PUERTO);
 
-            while (true) {
+            Thread consoleThread = new Thread(() -> {
+                Scanner scanner = new Scanner(System.in);
+                while (servidorActivo) {
+                    String comando = scanner.nextLine();
+                    if (comando.equalsIgnoreCase("SALIR")) {
+                        servidorActivo = false;
+                        cerrarServidor();
+                    }
+                }
+                scanner.close();
+            });
+            consoleThread.start();
+
+            while (servidorActivo) {
                 Socket clienteSocket = servidorSocket.accept();
-                ClienteHilo cliente = new ClienteHilo(clienteSocket);
-                clientes.add(cliente);
-                cliente.start();
+                if (servidorActivo) {
+                    ClienteHilo cliente = new ClienteHilo(clienteSocket);
+                    clientes.add(cliente);
+                    cliente.start();
+                } else {
+                    clienteSocket.close();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    static synchronized void cerrarServidor() {
+        for (ClienteHilo cliente : clientes) {
+            cliente.enviarMensaje("Servidor", "El servidor se ha cerrado.");
+            cliente.finalizarHilo();
+        }
+        clientes.clear();
+        topicos.clear();
+    }
+
 
     static synchronized void enviarMensaje(String topico, String mensaje, ClienteHilo remitente) {
         HashSet<ClienteHilo> suscriptores = topicos.get(topico);
@@ -63,6 +92,8 @@ class ClienteHilo extends Thread {
     private BufferedReader entrada;
     private PrintWriter salida;
 
+    private boolean hiloActivo = true;
+
     public ClienteHilo(Socket socket) {
         this.socket = socket;
         try {
@@ -72,12 +103,21 @@ class ClienteHilo extends Thread {
             e.printStackTrace();
         }
     }
-
+    public void finalizarHilo() {
+        hiloActivo = false;
+        try {
+            entrada.close();
+            salida.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void run() {
         try {
             String mensaje;
-            while ((mensaje = entrada.readLine()) != null) {
+            while (hiloActivo && (mensaje = entrada.readLine()) != null) {
                 String[] partes = mensaje.split(":");
                 String contenido = partes.length > 2 ? partes[2] : "";
 
